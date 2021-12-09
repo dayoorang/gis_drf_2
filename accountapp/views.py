0285ca9c4b19bd060958ffddf6bea525407b8c2b
dayoorang <dayoorang@gmail.com>
2021-12-09 13:24:19 +0900

    21/12/09 hello world view update

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def hello_world(request):
+    return Response({"message":"hello world"})
