ae0204b60d740aad96ce3830590811e3534ce2c3
dayoorang <dayoorang@gmail.com>
2021-12-10 16:16:29 +0900

    Create Api view template

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate
+from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView
 
 app_name = 'accountapp'
 
@@ -9,5 +9,9 @@ urlpatterns = [
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
-    path('create/',AccountCreateTemplate,name='create')
+
+
+    path('create_template/',AccountCreateTemplate,name='create_template'),
+
+    path('create/',AccountCreateAPIView.as_view(), name='create'),
 ]
