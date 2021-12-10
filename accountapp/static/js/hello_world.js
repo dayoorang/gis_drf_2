114693653aaf5f371543f354fe91d44a6aa0c78b
dayoorang <dayoorang@gmail.com>
2021-12-10 13:44:00 +0900

    serializer update

--- accountapp/static/js/hello_world.js
+++ accountapp/static/js/hello_world.js
@@ -6,7 +6,8 @@ function send_input() {
   .then(function (response) {
     console.log(response);
 
-    document.getElementById('text').innerHTML = response.data['message'];
+    document.getElementById('text').innerHTML = response.data['text'];
+    document.getElementById('new_model_created_at').innerHTML = response.data['created_at'];
 
   })
   .catch(function (error) {
@@ -23,7 +24,7 @@ axios.get('/accounts/hello_world/')
     console.log(response);
 
     // 성공했을 경우 어떤 javascript 작업을 할지
-    document.getElementById('text').innerHTML = response.data['message'];
+    document.getElementById('text').innerHTML = response.data['new_model'];
 
   })
   .catch(function (error) {
