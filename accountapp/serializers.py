bacc08619cc5e99de14eb358da549d08101bda95
dayoorang <dayoorang@gmail.com>
2021-12-14 09:37:13 +0900

    retrieve update

--- accountapp/serializers.py
+++ accountapp/serializers.py
@@ -14,3 +14,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data): # 비번 암호화 과정 위해서 사용
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class UserWithoutPasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','email','date_joined']
--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -7,13 +7,7 @@ function setCookie(name, value, options = {}) {
   };
 
   if (options.expires instanceof Date) {
-    options.expires = options.expires.toUTCString();
-  }
-
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-  for (let optionKey in options) {
-    updatedCookie += "; " + optionKey;
+    options.expires = opt
     let optionValue = options[optionKey];
     if (optionValue !== true) {
       updatedCookie += "=" + optionValue;
