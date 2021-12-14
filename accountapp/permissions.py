f7e0ff42c24d1ebbe993fe3b9935f6120af5c049
dayoorang <dayoorang@gmail.com>
2021-12-14 16:49:51 +0900

    permission modify

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
-    """
-    Allows access only to admin users.
-    """
+
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj == request.user
