3269a027e52323802e2b979b060867279ba3b47c
dayoorang <dayoorang@gmail.com>
2021-12-16 09:17:37 +0900

    profile permissions update

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView
+from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, ProfileUpdateAPIView
 
 app_name='profileapp'
 
 urlpatterns = [
     path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
     path('',ProfileCreateAPIView.as_view(), name='create'),
+    path('<int:pk>', ProfileUpdateAPIView.as_view(), name='update'),
+
 ]
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from profileapp.models import Profile
+from profileapp.permissions import IsProfileOwner
 from profileapp.serializers import ProfileSerializer
 
 
@@ -21,3 +22,10 @@ class ProfileCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class ProfileUpdateAPIView(UpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    permission_classes = [IsProfileOwner]
+    authentication_classes = [TokenAuthentication]
