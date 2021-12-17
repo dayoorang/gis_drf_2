bd9786e578305c9a6808008a6a09e1ed5df0c7fa
dayoorang <dayoorang@gmail.com>
2021-12-17 11:42:52 +0900

    article view update

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,7 +1,11 @@
+import sys
+
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
 # Create your models here.
+from image_processing.thumbnail import generate_thumbnail
 
 
 class Article(models.Model):
@@ -13,4 +17,15 @@ class Article(models.Model):
     content = models.TextField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.generate_thumbnail()
+        super().save(*args, **kwargs)
+
+    def generate_thumbnail(self):
+        if self.image:
+            output = generate_thumbnail(self.image)
+
+        self.thumb = InMemoryUploadedFile(output, "ImageField", self.image.name,
+                                          'image/jpeg',sys.getsizeof(output), None)
 
