f26ef3f69cdea02e98bb093639bed24b826301bf
dayoorang <dayoorang@gmail.com>
2021-12-22 16:12:48 +0900

    pem file git ignore update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,24 +1,19 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateAPIView, ArticleCreateTemplateView, ArticleRUDAPIView, \
-    ArticleRetrieveTemplateView, ArticleUpdateTemplateView, ArticleDestroyTemplateView, MagicGridTemplateView, \
-    ArticleListAPIView
-
-app_name='articleapp'
-
-
-
+from articleapp.views import ArticleCreateTemplateView, ArticleRUDAPIView, \
+    ArticleRetrieveTemplateView, ArticleUpdateTemplateView, ArticleDestroyTemplateView, ArticleListTemplateView, \
+    ArticleListCreateAPIView
+app_name = 'articleapp'
 
 urlpatterns = [
     path('create_template/', ArticleCreateTemplateView.as_view(), name='create_template'),
-    path('magic_grid/',MagicGridTemplateView.as_view(), name='magic_grid'),
+    path('list/', ArticleListTemplateView.as_view(), name='list_template'),
 
-    path('list/', ArticleListAPIView.as_view(), name='list'),
-    path('', ArticleCreateAPIView.as_view(), name = 'create'),
+    path('', ArticleListCreateAPIView.as_view(), name="LC"),
 
     path('retrieve_template/<int:pk>', ArticleRetrieveTemplateView.as_view(), name='retrieve_template'),
     path('update_template/<int:pk>', ArticleUpdateTemplateView.as_view(), name='update_template'),
     path('destroy_template/<int:pk>', ArticleDestroyTemplateView.as_view(), name='destroy_template'),
 
-    path('<int:pk>',ArticleRUDAPIView.as_view(), name='RUD' ),
-    ]
+    path('<int:pk>', ArticleRUDAPIView.as_view(), name='RUD'),
+]
--- articleapp/views.py
+++ articleapp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from articleapp.models import Article
@@ -16,36 +16,42 @@ from articleapp.serializers import ArticleSerializer
 class ArticleCreateTemplateView(TemplateView):
     template_name = 'articleapp/create.html'
 
-class MagicGridTemplateView(TemplateView):
-    template_name = 'articleapp/magic_grid.html'
 
-class ArticleListAPIView(ListAPIView):
+class ArticleListTemplateView(TemplateView):
+    template_name = 'articleapp/list.html'
+
+
+class ArticleListCreateAPIView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     authentication_classes = [TokenAuthentication]
     pagination_class = CustomPageNumberPagination
-
-class ArticleCreateAPIView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
-
 
     def perform_create(self, serializer):
         serializer.save(writer=self.request.user)
 
+    def post(self, request, *args, **kwargs):
+        """
+        Article 생성 Endpoint
+
+        게시글 생성하기 위한 로직<br>
+        게시글 생성하기 위해서는 로그인 권한을 요구합니다.
+        """
+        return super().post(self, request, *args, **kwargs)
+
 class ArticleRetrieveTemplateView(TemplateView):
     template_name = 'articleapp/retrieve.html'
+
 
 class ArticleUpdateTemplateView(TemplateView):
     template_name = 'articleapp/update.html'
 
+
 class ArticleDestroyTemplateView(TemplateView):
     template_name = 'articleapp/destroy.html'
+
 
 class ArticleRUDAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
@@ -63,7 +69,6 @@ class ArticleRUDAPIView(RetrieveUpdateDestroyAPIView):
         if request.user == target_article.writer:
             result_dict['is_page_owner'] = 'True'
         else:
-            result_dict['is_page_owner'] = 'False'
+            result_dict['is_page_owner'] = "False"
 
         return Response(result_dict)
-
