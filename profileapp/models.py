05fd8aa3210c6b1a258cf090adff31f7393a497b
dayoorang <dayoorang@gmail.com>
2021-12-23 16:26:46 +0900

    async update

--- profileapp/models.py
+++ profileapp/models.py
@@ -6,7 +6,11 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 from PIL import Image
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from image_processing.thumbnail import generate_thumbnail
+from profileapp.tasks import generate_thumbnail_celery_lag
 
 
 class Profile(models.Model):
@@ -18,13 +22,6 @@ class Profile(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        self.generate_thumbnail()
-        super().save(*args, **kwargs)
-
-    def generate_thumbnail(self):
-        if self.image:
-            output = generate_thumbnail(self.image)
-
-        self.thumb = InMemoryUploadedFile(output, "ImageField", self.image.name,
-                                          'image/jpeg',sys.getsizeof(output), None)
+@receiver(post_save, sender=Profile)
+def async_generate_thumbnail(sender, instance=None, created=False, **kwargs):
+    generate_thumbnail_celery_lag.delay(instance.pk)
